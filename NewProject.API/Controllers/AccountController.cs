0740e0f31b7e84fb84554920c1714e5ba1acd2de
nisha <nisha@DCS01>
2022-12-26 15:53:19 +0530

    Added some changes.

--- NewProject.API/Controllers/AccountController.cs
+++ NewProject.API/Controllers/AccountController.cs
@@ -1,10 +1,15 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using NewProject.API.Hubs;
 using NewProject.API.Requests.Payment;
 using NewProject.API.Requests.User;
+using NewProject.Domain.Entities.Payment;
 using NewProject.Services.Entities.User;
 using NewProject.Services.Interfaces;
 using NewProject.Utility;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.Net;
 using System.Text;
 
@@ -16,10 +21,12 @@ namespace NewProject.API.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IAuthenticationService _authenticationService;
-        public AccountController(IAuthenticationService authenticationService, IMapper mapper)
+        private readonly IHubContext<ChatHub> _hubContext;
+        public AccountController(IAuthenticationService authenticationService, IMapper mapper, IHubContext<ChatHub> hubContext)
         {
             _authenticationService = authenticationService;
             _mapper = mapper;
+            _hubContext = hubContext;
         }
         [HttpPost("authenticate")]
         public async Task<Dictionary<string, object>> AuthenticateAsync([FromBody] UserAuthRequest request)
@@ -42,9 +49,23 @@ namespace NewProject.API.Controllers
         [HttpGet("signalRTest")]
         public async Task<string> aa(string user, string message)
         {
+            SignalRclass signalRclass = new SignalRclass()
+            {
+                user = user,
+                message = message
+            };
+            var json = JsonConvert.SerializeObject(signalRclass);
+
+           // SendAsync("broadcastAppointment", JsonConvert.SerializeObject(json, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }));
             var connection = new NewProject.API.Hubs.ChatHub();
-            await connection.SendMessageToAll(user, message);
+            await _hubContext.Clients.All.SendAsync(json);
             return message;
         }
+        public class SignalRclass
+        {
+            public string user { get; set; }
+            public string message { get; set; }
+        }
+
     }
 }
